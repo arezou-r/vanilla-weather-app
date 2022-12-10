1e3e3f04327aacc66de44cffbafce5d23aa66ab3
arezou-r <arezou.ruhi@gmail.com>
2022-12-10 22:40:26 +0330

    Added forecast data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-let days = ["sunday", "monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let forecastDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 function formatDate(timestamp) {
     let date = new Date(timestamp);
@@ -16,6 +17,9 @@ function formatDate(timestamp) {
 }
 
 
+// let apiKey = "7059cb165caa3316bff682d263a01b1e";
+let apiKey2 = "3794cado846aa024tdda83fba00effa9";
+let apiKey = "d57ba12fe2c36fb7d6e4a542d490147c";
 let cityNameEmelent = document.querySelector("#city");
 let descriptionEmelent = document.querySelector("#description");
 let tempElement = document.querySelector("#temperature");
@@ -27,6 +31,45 @@ let formElement = document.querySelector("#search_form");
 let cityInputElement = document.querySelector("#city_input");
 let farLinkElement = document.querySelector("#fahrenheit_link");
 let celsiusLinkElement = document.querySelector("#celsius_link");
+
+
+let forecastElement = document.querySelector("#forecast .row");
+
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = forecastDays[date.getDay()]
+    return day;
+}
+
+function displayForecast(response) {
+    console.log(response)
+    let forcastData = response.data.daily;
+    let forecastHtml = "";
+    forcastData.forEach(function(forecastDay, index) {
+        if (index < 6) {
+            forecastHtml = forecastHtml + `
+                <div class="col">
+                    <div class="forecast_box">
+                        <p class="forecast_date">${formatDay(forecastDay.time)}</p>
+                        <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.condition.icon}.png" class="forecast_icon" alt="img">
+                        <div class="forecast_temp">
+                            <span class="forcast_temp_max">${Math.round(forecastDay.temperature.maximum)}° </span>
+                            <span class="forcast_temp_min">${Math.round(forecastDay.temperature.minimum)}°</span>
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
+    });
+
+    forecastElement.innerHTML = forecastHtml;
+}
+
+function getForecast(cityName) {
+    // let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${cityName}&key=${apiKey2}&units=metric`
+    axios.get(apiUrl).then(displayForecast);
+}
 
 function displayInfo(response) {
     console.log(response);
@@ -40,10 +83,12 @@ function displayInfo(response) {
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", `${response.data.weather[0].description}`);
+
+    getForecast(response.data.name);
+
 }
 
 function search(city) {
-    let apiKey = "d57ba12fe2c36fb7d6e4a542d490147c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayInfo);
 }
